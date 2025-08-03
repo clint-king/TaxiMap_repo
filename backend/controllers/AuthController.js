ae6cbf1462468ecf15ad6bd0e00fdbfd03370af5
clint-king <bambiclietin@gmail.com>
2025-08-03 15:17:00 +0200

    fixing assignment instead of comparing

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -32,21 +32,21 @@ export const login = async (req, res) => {
     // Optional: Generate a JWT
     const token = jwt.sign({ id: user.id , user_type: user.user_type}, 'secret_key', { expiresIn: '1h' });
 
-    if(process.env.NODE_ENV == 'production'){
+   // if(process.env.NODE_ENV == 'production'){
   res.cookie('token', token, {
     httpOnly: true,
     secure: true, // set to true in production (requires HTTPS)
     sameSite: 'None',
     maxAge: 3600000
   });
-    }else{
-    res.cookie('token', token, {
-    httpOnly: false,
-    secure: true, 
-    sameSite: 'strict',
-    maxAge: 3600000
-  });
-    }
+  //   }else{
+  //   res.cookie('token', token, {
+  //   httpOnly: false,
+  //   secure: true, 
+  //   sameSite: 'strict',
+  //   maxAge: 3600000
+  // });
+  //   }
   
     return res.json({ message: 'Login successful' , user_type: user.user_type});
   } catch (error) {
