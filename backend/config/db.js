4dbbd38fbb1d11ec0a4d7b16750ef0ffcc864bd9
clint-king <bambiclietin@gmail.com>
2025-08-04 14:36:27 +0200

    ui_container fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,17 +6,17 @@ dotenv.config();
 
 let pool = null;
 console.log("NODE_ENV in db.js: " , process.env.NODE_ENV);
-// if(process.env.NODE_ENV == 'development'){
-//   pool = await mysql.createPool({
-//   host: 'localhost',
-//   user: 'TaxiMap_database',
-//   password: '12345',
-//   database: 'taximapdb',
-//   waitForConnections: true,
-//   connectionLimit: 10,  // for example, adjust as needed
-//   queueLimit: 0
-// });
-// }else{
+if(process.env.NODE_ENV == 'development'){
+  pool = await mysql.createPool({
+  host: 'localhost',
+  user: 'TaxiMap_database',
+  password: '12345',
+  database: 'taximapdb',
+  waitForConnections: true,
+  connectionLimit: 10,  // for example, adjust as needed
+  queueLimit: 0
+});
+}else{
  pool = await mysql.createPool({
   host: 'turntable.proxy.rlwy.net',
   port: 16301,
@@ -27,7 +27,7 @@ console.log("NODE_ENV in db.js: " , process.env.NODE_ENV);
   connectionLimit: 10,  // for example, adjust as needed
   queueLimit: 0
 });
-//}
+}
 
 
 console.log("Connection to database is Successful");
