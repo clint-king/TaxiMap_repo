8f21b10c20576a08c5e521c81be99bf3b4e40a29
clint-king <bambiclietin@gmail.com>
2025-08-06 17:57:12 +0200

    make sure client.html can resize into different screen sizes

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,27 +6,27 @@ dotenv.config();
 
 let pool = null;
 console.log("NODE_ENV in db.js: " , process.env.NODE_ENV);
-// if(process.env.NODE_ENV == 'development'){
-//   pool = await mysql.createPool({
-//   host: 'localhost',
-//   user: 'TaxiMap_database',
-//   password: '12345',
-//   database: 'taximapdb',
-//   waitForConnections: true,
-//   connectionLimit: 10,  // for example, adjust as needed
-//   queueLimit: 0
-// });
-// }else{
- pool = await mysql.createPool({
-  host: 'turntable.proxy.rlwy.net',
-  port: 16301,
-  user: 'root',
-  password: 'BQGqyvPPCWSjCHKCIjRQsaobCexagOdQ',
-  database: 'railway',
+//if(process.env.NODE_ENV == 'development'){
+  pool = await mysql.createPool({
+  host: 'localhost',
+  user: 'TaxiMap_database',
+  password: '12345',
+  database: 'taximapdb',
   waitForConnections: true,
   connectionLimit: 10,  // for example, adjust as needed
   queueLimit: 0
 });
+// }else{
+//  pool = await mysql.createPool({
+//   host: 'turntable.proxy.rlwy.net',
+//   port: 16301,
+//   user: 'root',
+//   password: 'BQGqyvPPCWSjCHKCIjRQsaobCexagOdQ',
+//   database: 'railway',
+//   waitForConnections: true,
+//   connectionLimit: 10,  // for example, adjust as needed
+//   queueLimit: 0
+// });
 //}
 
 
